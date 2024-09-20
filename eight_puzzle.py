c7c8bc748de141cf988ea6d9d1579f9571997243
Catherine Tran <catherinetran04@gmail.com>
2024-09-19 20:08:43 -0700

    Update eight_puzzle.py

--- eight_puzzle.py
+++ eight_puzzle.py
@@ -2,66 +2,40 @@ import sys
 sys.path.append('aima-python')
 from search import *
 import math
+import time
 
-class HW2:
+class EightP:
 
     def __init__(self):
         pass
 
-    def example_problem(self):
-        #EightPuzzle example with A*
-        # Default goal is (1, 2, 3, 4, 5, 6, 7, 8, 0)
-        #   which represents:   1 2 3
-        #                       4 5 6
-        #                       7 8 _
-        #
-
-        # In this example, we'll construct a puzzle with initial state
-        #               1 2 3
-        #               4 5 6
-        #               _ 7 8
-        #
+    def eight_puzzle_init(self):
+        '''
+        1. create an instance of the 8-puzzle with initial state (1, 2, 3, 4, 5, 6, 0, 7, 8) 
+        2. check solbaility and run A* search on it with the default heuristic
+        2. return the solution from the A* search algorithm
+        '''
         init = (1, 2, 3, 4, 5, 6, 0, 7, 8)
         puzzle = EightPuzzle(init)
         print("Is the puzzle solvable from this initial state?")
         print(puzzle.check_solvability(init))
-        # Checks whether the initialized configuration is solvable or not
         print("A* with default heuristic")
         return astar_search(puzzle).solution()
 
-    def problem_1a(self):
-        '''
-        1. instantiate the search algorithm with the 8 puzzle problem
-            as described in the writeup
-        2. return the solution from the A* search algorithm
-        '''
-        init = (1, 6, 3, 4, 0, 8, 2, 7, 5)
-        puzzle = EightPuzzle(init)
-        return astar_search(puzzle).solution()
     
-    def problem_1b(self):
+    
+    def eight_puzzle_init_goal(self):
         '''
-        1. instantiate the search algorithm with the 8 puzzle problem
-            as described in the writeup
+        1. create an instance of the 8-puzzle with initial state (0, 6, 2, 1, 5, 3, 4, 8, 7) 
+        and goal state (1, 2, 3, 8, 0, 4, 7, 6, 5) and run A* search on it with the default heuristic
         2. return the solution from the A* search algorithm
         '''
-        init =  (1, 4, 2, 6, 7, 3, 0, 8, 5) 
+        init =  (0, 6, 2, 1, 5, 3, 4, 8, 7) 
         goal =   (1, 2, 3, 8, 0, 4, 7, 6, 5)
         puzzle = EightPuzzle(init,goal)
         return astar_search(puzzle).solution() 
 
-    def problem_1c(self):
-        '''
-        1. instantiate the search algorithm with the 8 puzzle problem
-            as described in the writeup
-        2. return the solution from the A* search algorithm
-        '''
-        init =  (0, 6, 2, 1, 5, 3, 4, 8, 7)
-        goal =   (1, 2, 3, 8, 0, 4, 7, 6, 5) 
-        puzzle = EightPuzzle(init, goal)
-        return astar_search(puzzle).solution() 
-
-    def problem_1d(self):
+    def eight_puzzle_with_manhattan_distance_heuristic(self):
         '''
         1. instantiate the search algorithm with the 8 puzzle problem 
         2. write code that will create a different heuristic
@@ -69,13 +43,7 @@ class HW2:
         '''
         
         init = (0, 6, 2, 1, 5, 3, 4, 8, 7)
-        # 0  6  2  
-        # 1  5  3 
-        # 4  8  7
         goal = (1, 2, 3, 8, 0, 4, 7, 6, 5)
-        # 1  2  3
-        # 8  0  4 
-        # 7  6  5
         puzzle = EightPuzzle(init, goal)
         node = Node(init)
         def mhd(node):
@@ -93,33 +61,22 @@ class HW2:
             return sum
         return astar_search(puzzle, mhd).solution()
 
-    def problem_1e(self):
+    def comparing_heuristics(self):
         '''
         1. find initial states with optimal solutions of lengths 18, 20, 22 and 24
-        2. for each of those, for each heuristic, measure the time it takes to find a solution
-        Note: It is not required that all of your code for this be done specifically in this 
-        function. It can be elsewhere in the file if you want to structure the code differently
-        The autograder will not test this code, but we will look at it by hand, so if it
-        is not all in this function, leave a comment letting us know where to look.
+        2. for each of those, for each heuristic measure the time it takes to find a solution
+
+        Find four initial states that have optimal solutions of lengths 18, 20, 22 and 24
+        For each of these, run A* search using the  default heuristic and your heuristic
+        Use a python function or unix utility to time the length of each of the runs.
+        In your writeup, create a table with the eight timing results. 
         '''
         goal = (0, 1, 2, 3, 4, 5, 6, 7, 8)
-        #init24 = (2, 7, 8, 1, 6, 4, 0, 5, 3)
-        #puzzle24 = EightPuzzle(init24)
-        #node = Node(init24)
-
-        #init22 = (1, 5, 2, 3, 4, 7, 6, 8, 0)
-        #puzzle22 = EightPuzzle(init22)    
-        #node = Node(init22)
-
-        #init20 = (1, 6, 2, 3, 4, 5, 7, 8, 0)
-        #puzzle20 = EightPuzzle(init20)
-        #node = Node(init20)
-
-
         init18 = (1, 3, 2, 4, 6, 5, 7, 8, 0)
         puzzle18 = EightPuzzle(init18)
         node = Node(init18)
 
+        # same as above
         def mhd(node):
             init = node.state
             '''Define your manhattan-distance heuristic for the 8-puzzle here
@@ -135,38 +92,18 @@ class HW2:
             return sum
 
 
-        #print (len(astar_search(puzzle24).solution()))
-        #print (astar_search(puzzle24).solution())
-        #print (astar_search(puzzle24, mhd).solution())
-        #print('==================')
-
-
-        #print (len(astar_search(puzzle22).solution()))
-        #print (astar_search(puzzle22).solution())
-        #print (astar_search(puzzle22, mhd).solution())
-        #print('==================')
-
-
-        #print (len(astar_search(puzzle20).solution()))
-        #print (astar_search(puzzle20).solution())    
-        #print (astar_search(puzzle20, mhd).solution())      
-        #print('==================')
-
-
         print (len(astar_search(puzzle18).solution()))
+        #default_time = time.time()
+        #manhattan_time = time.time()
         print (astar_search(puzzle18).solution()) 
         print (astar_search(puzzle18, mhd).solution())
+        #print (default_time, manhattan_time)
         print('==================')
-
-
-
-
-        #return astar_search(puzzle).solution()
         return 0
 
 
 
-    def example_problem_2(self):
+    def romania_map_bfs(self):
         '''Use the InstrumentedProblem class to track stats about a breadth-first
         search on the Romania Map problem.
         '''
@@ -179,7 +116,7 @@ class HW2:
         print(g)
         return g
 
-    def problem_2a(self):
+    def romania_map_bfs_dfs_ids(self):
         '''Use the InstrumentedProblem class to track stats about 
         different searches on the Romania Map problem.
         '''
@@ -195,7 +132,7 @@ class HW2:
         print(g3)
         return (g1, g2, g3)
 
-    def problem_2b(self):
+    def romania_map_ids_uc_astar(self):
         '''Use the InstrumentedProblem class to track stats about
         different searches on the Romania Map problem.
         '''
@@ -214,47 +151,39 @@ class HW2:
 
 def main():
     
-    # Create object, hw2, of datatype HW2.
-    hw2 = HW2()
+    # Create object, ep, of datatype EightP.
+    ep = EightP()
  
     #=======================
     # A* with 8-Puzzle 
     # An example for you to follow to get you started on the EightPuzzle
-    print('Example Problem result:')
+    print('Eight puzzle with only initial state input:')
     print('=======================')
-    print(hw2.example_problem())
-    
-    print('Problem 1a result:')
-    print('==================')
-    print(hw2.problem_1a())
+    print(ep.eight_puzzle_init())
 
-    print('Problem 1b result:')
+    print('Eight puzzle given intitial state and goal:')
     print('==================')
-    print(hw2.problem_1b())
-
-    print('Problem 1c result:')
-    print('==================')
-    print(hw2.problem_1c())
+    print(ep.eight_puzzle_init_goal())
 
     print('Problem 1d result:')
     print('==================')
-    print(hw2.problem_1d())
+    print(ep.eight_puzzle_with_manhattan_distance_heuristic())
 
-    #print('Problem 1e result:')
-    #print('==================')
-    #print(hw2.problem_1e())
+    print('Problem 1e result:')
+    print('==================')
+    print(ep.comparing_heuristics())
 
     print('Problem 2 example')
     print('==================')
-    print(hw2.example_problem_2())
+    print(ep.romania_map_bfs())
 
     print('Problem 2a result:')
     print('==================')
-    print(hw2.problem_2a())
+    print(ep.romania_map_bfs_dfs_ids())
 
     print('Problem 2b result:')
     print('==================')
-    print(hw2.problem_2b())
+    print(ep.romania_map_ids_uc_astar())
 
     
 if __name__ == '__main__':
